0d6042b3dc7e95292a9fe326f1ee7f4b1a1cdc31
daberpro <daber.programing@gmail.com>
2022-02-10 21:52:17 +0700

    resolve path

---
+++ esbuild-plugin.seleku-kit.js
@@ -0,0 +1,24 @@
+const { Compile } = require("./experimental-compiler.js");
+const fs = require("fs");
+const { basename } = require("path");
+const url = require("url");
+
+module.exports.dabComPlugin = {
+    name: 'dabCom-esbuild-plugin',
+    setup(build) {
+
+        build.onLoad({ filter: /\.selek$/}, async (args) =>{
+
+            const time = Date.now();
+
+            let text = await fs.promises.readFile(args.path, 'utf8')
+
+            console.log(basename(url.parse(args.path).pathname),Math.abs(time - Date.now())+"ms");
+            
+            return {
+                contents: Compile(text).JS
+            }
+
+        })
+    }
+}
